ce226aff95a48cfaf3dc7725ec971ec84e8eb5dc
Rohit-Bharti0058 <rohit1bharti10058@gmail.com>
2020-03-18 19:18:54 +0530

    SignUp_SignIn_Working_App_with_Angular_9_Express_MongoDB_and_NodeJS

---
+++ Login_Logout_and_Registration_App_with_MEAN_Stack/AngularApp/src/app/shared/user.service.ts
@@ -0,0 +1,66 @@
+import { Injectable, APP_ID } from '@angular/core';
+
+import { User } from './user.model';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+//import { localizedString } from '@angular/compiler/src/output/output_ast';
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  selectedUser: User = {
+   fullName: '',
+   email: '',
+   password: ''
+  };
+  noAuthHeader =  {headers : new HttpHeaders({'NoAuth': 'True'})};
+
+  constructor(private http: HttpClient ) { }
+
+  postUser(user : User)
+  {
+    return this.http.post(environment.apiBaseUrl +'/register',user,this.noAuthHeader);
+  }
+  login(authCredentials)
+  {
+    return this.http.post(environment.apiBaseUrl +'/authenticate',authCredentials,this.noAuthHeader);
+  }
+  setToken(token: string)
+  {
+    localStorage.setItem('token',token);
+  }
+  deleteToken()
+  {
+    localStorage.removeItem('token');
+  }
+  getToken()
+  {
+    return localStorage.getItem('token');
+  }
+  getUserProfile()
+  {
+    return this.http.get(environment.apiBaseUrl + '/userProfile');
+  }
+  getUserPayload()
+    {
+      var token=this.getToken();
+      if(token)
+      {
+        var userPayload=atob(token.split('.')[1]);
+        return JSON.parse(userPayload);
+      }
+      else
+      return null;
+    }
+   
+    isLoggedIn()
+    {
+      var userPayload = this.getUserPayload();
+      if(userPayload)
+      return userPayload.exp > Date.now() / 1000;
+      else
+      return null;
+    }
+  
+}
